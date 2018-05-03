55d5ae3c78c7575a6cbd365739efd3d2ef8781de
threetreeslight <ae06710@gmail.com>
2018-05-03 16:22:40 +0900

    fix(email): impl automatically ellipsis email history

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # refine-intercom
+
+Features
+
+1. [automatically ellipsis email history](docs/automatically_ellipsis_email_history.png)
+
+
+
+## Getting Started
+
+Must Read https://developer.chrome.com/extensions/getstarted
+
+```
+git clone git@github.com:threetreeslight/refined-intercom.git
+```
+
---
+++ ellipsisEmailHistory.js
@@ -0,0 +1,37 @@
+const kMailStartPhrases = ["-----Original Message-----", "&gt; &gt;"]
+
+var ellipsisEmailHistory = function() {
+  var conversations = document.getElementsByClassName('conversation__text');
+  for (var conversation of conversations) {
+    var replaced_html = conversation.innerHTML;
+
+
+    // if already details exists, skip loop
+    // when history opened, details tag append `open` attribute.
+    // So, it checks `<details` prefix.
+    if (replaced_html.includes("<details", "")) {
+      continue
+    }
+
+    // find startline and prepend details tag
+    var idx = kMailStartPhrases.reduce(function (acc, val, _, _) {
+        return acc == -1 ? replaced_html.indexOf(val) : acc
+      },
+      -1
+    )
+
+    // it doesn't have email history, skip loop
+    if (idx == -1) {
+      continue
+    }
+
+    // if prepended details tag. append close details tag at least.
+    replaced_html = replaced_html.slice(0,idx) + "<details><summary>email history</summary>" + replaced_html.slice(idx, -1) + "</details>";
+    // replace p tag to br to work details tag
+    replaced_html = replaced_html.replace(new RegExp("<(p|\/p)>", "g"), "<br>");
+
+    conversation.innerHTML = replaced_html;
+  }
+}
+
+setInterval( ellipsisEmailHistory, 3000 );
